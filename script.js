c363de1baf6ce0768930384474c472912daf8b16
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-06 15:42:46 -0400

    added images and javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+// www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}
+
+var ingredient = "";
+var apiKey = 1;
+
+var buttonEl = $('.btn');
+
+buttonEl.on("click", function () {
+    console.log("test");
+});
