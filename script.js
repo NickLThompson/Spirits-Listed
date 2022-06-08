686a3c6e43919d6fd8270c8c5bf2227e8cc65e63
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-06-08 10:20:06 -0400

    added 2nd html page

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 // www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}
-var buttonEl = $('.btn');
+$(document).ready(function () {
 
-var ingredient = [];
+    var buttonEl = $(".btn");
 
+    var searchBtnEl = $("#searchBtn");
 
+    var ingredient = [];
 
+    var chosenIngredient = [];
 
+    var input = $(".checkbox");
+    // compiles user ingredients into array to search API for drinks
+    var getSearchResults = function (element) {
+        // adds selected ingredient to array whenever button is clicked
+        if (element.target.checked === true) {
+            ingredient.push(element.target.value);
+            // removed unselected ingrdient from array whenever button is unclicked
+        } else {
+            ingredient.splice(ingredient.indexOf(element.target.value), 1);
+        } return chosenIngredient.push(ingredient);
 
+    }
 
+    var fetchDrinks = function () {
+        for (var i = 0; i < chosenIngredient.length; i++) {
+            fetch(`www.thecocktaildb.com/api/json/v1/1/filter.php?i=${chosenIngredient[i]}`)
+            .then(function (response) { 
+                return response.json();
+                console.log(response.json());
+            })
+            // return response.json();
+        }
+        console.log("search");
+    };
+    // checks the checkbox for clicks
+    input.on("click", getSearchResults);
 
+    buttonEl.on("click", function () {
 
+    });
 
+    searchBtnEl.on("click", fetchDrinks)
 
-
-
-
-
-
-
-
-
-
-
-buttonEl.on("click", function () {
-    console.log("test");
 });
+
+    // fetch("www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input}")
