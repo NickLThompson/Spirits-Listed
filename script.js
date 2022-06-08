31393ed7e4249c15f4338da05af76d328e845ccf
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-08 10:11:59 -0400

    prepare for pull

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 // www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}
-var buttonEl = $('.btn');
 
-var ingredient = [];
+var ingredient = "";
+var apiKey = 1;
 
+var buttonEl = $('btn');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-buttonEl.on("click", function () {
+buttonEl.on("click", function() {
     console.log("test");
-});
+})
